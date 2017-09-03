6c370dc71a65e53e1d2a27e70307cdd5a9dbad46
nramiscal <nramiscal@gmail.com>
2017-09-03 19:50:42 -0400

    first commit

---
+++ HumanTest.java
@@ -0,0 +1,10 @@
+public class HumanTest {
+    public static void main(String[] args){
+        Human h1 = new Human("Bob");
+        Human h2 = new Human("Ed");
+
+        h1.attack(h2);
+
+
+    }
+}
